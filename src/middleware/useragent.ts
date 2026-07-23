cdb6d05f07c36c87029df2931e3fedd87ac0bf6b
Alexander Moomaw <108152211+alhumaw@users.noreply.github.com>
2026-07-23 10:35:23 -0700

    chore: bump version to 0.7.0 (#324)

--- src/middleware/useragent.ts
+++ src/middleware/useragent.ts
@@ -8,7 +8,7 @@ export class UserAgent {
                 ...context.init,
                 headers: {
                     ...context.init.headers,
-                    "User-Agent": "falconjs/0.6.0",
+                    "User-Agent": "falconjs/0.7.0",
                 },
             },
         };
